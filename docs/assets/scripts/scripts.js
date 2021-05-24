5d291d04eef14f875c25436f06ebb5da1104c9eb
Domenico Lodola <dom@Domenicos-MacBook.local>
2021-05-24 23:46:32 +0100

    Update scripts.js

--- docs/assets/scripts/scripts.js
+++ docs/assets/scripts/scripts.js
@@ -1,10 +1,10 @@
 function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+    // var x = document.getElementById("myDIV");
+    // if (x.style.display === "none") {
+    //   x.style.display = "block";
+    // } else {
+    //   x.style.display = "none";
+    // }
 
     var tags = document.getElementsByClassName('tag');
     for (var i = 0; i < tags.length; i ++) {
