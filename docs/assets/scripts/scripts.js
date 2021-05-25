2e6b5f145e155442c779d66e3478fb882e60403c
Domenico Lodola <dom@Domenicos-MacBook.local>
2021-05-25 22:45:24 +0100

    Update scripts.js

--- docs/assets/scripts/scripts.js
+++ docs/assets/scripts/scripts.js
@@ -19,17 +19,12 @@ $(window).bind('hashchange', function (e) {
 function getContent() {
     var url = window.location.toString();
     var hash = url.substring(url.indexOf('#'));
-    $('html, body').animate({
-        scrollTop: $(hash).offset().top
-    }, 2000);
-    $(hash).fadeIn();
-    $(hash).siblings("div").fadeOut();
 
 
 
   var tags = document.getElementsByClassName('tag');
   for (var i = 0; i < tags.length; i ++) {
       tags[i].style.display = 'none';
-
+  }
   document.getElementById(hash).style.display = "block";
 }
